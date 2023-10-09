641fcd19ac9cff5680d0b97cb1fd22e2e85c56d7
Ariela Israel <arielaisrael12@gmail.com>
2023-10-09 17:12:39 -0400

    former form restored

--- src/views/CreateDeck/index.js
+++ src/views/CreateDeck/index.js
@@ -1,55 +1,78 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import Breadcrumb from '../../common/TwoItemBreadcrumb';
 import { createDeck } from '../../utils/api';
-import CustomForm from '../../common/CustomForm';
 
 function CreateDeck({ history, handleDeckCreation }) {
+	const [deckName, setDeckName] = useState('');
+	const [deckDescription, setDeckDescription] = useState('');
 
-	const deckFields = [
-		{
-			label: 'Name',
-			id: 'deck-name',
-			name: 'deckName',
-			placeholder: 'Deck Name',
-		},
-		{
-			label: 'Description',
-			id: 'deck-description',
-			name: 'deckDescription',
-			type: 'textarea',
-			placeholder: 'Brief description of deck',
-		},
-	];
+	useEffect(() => {
+		return () => {};
+	}, []);
 
-	const submitDeckHandler = async formData => {
+	const submitDeckHandler = async e => {
+		e.preventDefault();
+
 		try {
 			const newDeck = {
-				name: formData.deckName,
-				description: formData.deckDescription,
+				name: deckName,
+				description: deckDescription,
 			};
 
 			const createdDeck = await createDeck(newDeck);
 
-			handleDeckCreation(createdDeck);
+			handleDeckCreation(createDeck);
 
 			history.push(`/decks/${createdDeck.id}`);
 		} catch (err) {
 			console.error('Error adding new deck:', err);
 		}
 	};
-
 	return (
 		<div>
 			<Breadcrumb deckName={'Create Deck'} />
 			<div>
 				<h1>Create Deck</h1>
-				<CustomForm
-					onSubmit={submitDeckHandler}
-					fields={deckFields}
-				/>
+				<form onSubmit={submitDeckHandler}>
+					<div className='mb-3'>
+						<label
+							htmlFor='deck-name'
+							className='form-label'
+						>
+							Name
+						</label>
+						<input
+							type='text'
+							className='form-control'
+							id='deck-name'
+							placeholder='Deck Name'
+							value={deckName}
+							onChange={e => setDeckName(e.target.value)}
+						/>
+					</div>
+					<div className='mb-3'>
+						<label
+							htmlFor='deck-description'
+							className='form-label'
+						>
+							Description
+						</label>
+						<textarea
+							className='form-control'
+							id='deck-description'
+							rows='4'
+							placeholder='Brief description of deck'
+							value={deckDescription}
+							onChange={e => setDeckDescription(e.target.value)}
+						></textarea>
+					</div>
+					<div>
+						<button className='btn btn-secondary btn-1'>Cancel</button>
+						<button className='btn btn-primary'>Submit</button>
+					</div>
+				</form>
 			</div>
 		</div>
 	);
 }
-
 export default CreateDeck;
